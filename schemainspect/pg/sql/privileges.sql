ed300d2b113fa295b38a7dad0770e965ecd3f68f
Nathan Cahill <nathan@nathancahill.com>
2020-05-06 17:09:11 +0200

    add view privileges

--- schemainspect/pg/sql/privileges.sql
+++ schemainspect/pg/sql/privileges.sql
@@ -6,10 +6,15 @@ select
   privilege_type as privilege
 from information_schema.role_table_grants
 where grantee != (
-    select tableowner
+    select tableowner as owner
     from pg_tables
     where schemaname = table_schema
     and tablename = table_name
+    UNION
+    select viewowner as owner
+    from pg_views
+    where schemaname = table_schema
+    and viewname = table_name
 )
 -- SKIP_INTERNAL and table_schema not in ('pg_internal', 'pg_catalog', 'information_schema', 'pg_toast')
 -- SKIP_INTERNAL and table_schema not like 'pg_temp_%' and table_schema not like 'pg_toast_temp_%'
